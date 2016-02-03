0ad7d7d5c586e40f25535472069720f766930d48
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-02-03 17:49:40 +0000

    Added db to index routes to ensure working

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// Database connection
+var db = require("../db");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ title: 'nodejs sample api' });
